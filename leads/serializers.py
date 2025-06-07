beb8244c9c9b91e29530937c4d84b9461d658da6
anirudhj2000 <anirudhjoshi985@gmail.com>
2025-06-07 20:04:33 +0530

    api changes

--- leads/serializers.py
+++ leads/serializers.py
@@ -4,6 +4,7 @@ class LeadSerializer(serializers.Serializer):
    
     first_name = serializers.CharField(max_length=100)
     last_name = serializers.CharField(max_length=100)
+    email = serializers.EmailField()
     phone = serializers.CharField(max_length=15, required=False,)
     purpose = serializers.CharField(max_length=255, required=False)
     budget = serializers.CharField(max_length=50, required=False)

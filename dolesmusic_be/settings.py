f9118a24c85940401b5aac454bfd032907309027
anirudhj2000 <anirudhjoshi985@gmail.com>
2025-08-25 03:51:42 +0530

    fix for settings

--- dolesmusic_be/settings.py
+++ dolesmusic_be/settings.py
@@ -134,7 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"   # All static files collected here
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
